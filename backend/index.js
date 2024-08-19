2df3666d9527c0d556d6385f30ab98dc0b771f32
Gobinda B <gobinda2001b@gmail.com>
2024-08-19 23:24:43 +0530

    updated gitignore

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express();
 const PORT = process.config.PORT || 8080
 
+app.get("/", (req, res)=>{
+    res.send('hello world')
+})
+
 app.listen( PORT, (req, res)=>{
     console.log(`server started on port ${PORT}`)
 } )
